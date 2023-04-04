b7e2cb78fb695b258245e73f3bfd82185409423b
pradeepodela <72432569+pradeepodela@users.noreply.github.com>
2023-04-04 16:24:44 +0530

    Add files via upload
    Updated the code in app.py

--- send.py
+++ send.py
@@ -2,5 +2,5 @@ import telepot
 
 def sendpicture(message,image):
     bot = telepot.Bot('5981392455:AAHbYGVYJ1P9XntMvDfPDWzVbrRd-Lb7jJs')
-    bot.sendPhoto(5053711674, photo=open(img, 'rb'))
+    bot.sendPhoto(5053711674, photo=open(image, 'rb'))
     bot.sendMessage(5053711674, message)
